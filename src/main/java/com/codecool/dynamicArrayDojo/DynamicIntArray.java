3e403c7769d8f6b28bc947cb929457018ed422e5
monzag <zaganiacz.m@gmail.com>
2018-01-26 19:16:10 +0100

    Create structure of DynamicIntArray class

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -3,4 +3,29 @@ package com.codecool.dynamicArrayDojo;
 // put your code here!
 public class DynamicIntArray {
 
+    private int size;
+    private int[] staticArray;
+
+    public DynamicIntArray() {
+        this.size = 0;
+        this.staticArray = new int[size];
+    }
+
+    public DynamicIntArray(int size) {
+        this.size = size;
+        this.staticArray = new int[size];
+    }
+
+    public void add(int item) {
+
+    }
+
+    public void insert(int index, int item) {
+
+    }
+
+    public void remove(int index) {
+
+    }
+
 }
